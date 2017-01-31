546e4ed446b523d635a1b7f8e06388fc66c64cab
Juan Guillermo Romeo Monreal <juan.romeo.monreal@outlook.com>
2017-01-31 20:42:27 +0100

    Prod implementation MyConsole and MyTime

---
+++ MyTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OhceKata
+{
+    public class MyTime : ITime
+    {
+        public DateTime currentTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
