76d015121d79ce13e1a780faef44f90409390b72
Juan Guillermo Romeo Monreal <juan.romeo.monreal@outlook.com>
2017-02-05 17:52:33 +0100

    added ohce program which was not added

---
+++ OhceProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using OhceKata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OhceProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+            MyTime time = new MyTime();
+            MyConsole console = new MyConsole();
+            Action exitAction = () => { run = false; };
+            Ohce ohce = new Ohce(time, console, exitAction);
+
+            ohce.Greet();
+
+            while (run)
+            {
+                ohce.Execute();
+            }
+        }
+    }
+}
